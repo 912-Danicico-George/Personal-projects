d96640c7d1cf328a3ea2758602c8d715fbd97fc6
912-Danicico-George <cs.cody@yahoo.com>
2020-12-14 18:27:56 +0200

    Algebra project
    This program finds all the matrixes with size m,n in Z2 in reduced echelon form

---
+++ project_algebra/main.py
@@ -0,0 +1,112 @@
+# This is project 7 for Algebra Danicico George-Iulian
+
+# Input for the problem: m,n for a M2,3(Z2)
+
+# Output:
+# 1.The number of echelon form matrix of size m,n
+# 2. The matrixes in echelon form of size m,n in Z2
+
+
+# This is the initialisation of the matrix
+def init_matrix():
+    M = int(input("Enter the number of rows: "))
+    N = int(input("Enter the number of columns: "))
+    Matrix = []
+    for i in range(M):
+        appending_list = []
+        for j in range(N):
+            appending_list.append(0)
+        Matrix.append(appending_list)
+
+    return N, M, Matrix
+
+def print_solution(matrix, n, m, list_of_echelon_matrixes):
+    done = True
+    # the implementation below is to check if a matrix is indeed in a reduced echelon form
+    # if done will be True after the execution, the matrix is indeed in reduced echelon form
+    # otherwise, if done will be False it means that the matrix is not in reduced echelon form
+    for i in range(m):
+        previous_row_value = None
+        current_row_value = None
+        if i > 0:
+            for j in range(n):
+                if matrix[i - 1][j] == 1:
+                    if previous_row_value is None:
+                        previous_row_value = j
+
+                if matrix[i][j] == 1:
+                    if current_row_value is None:
+                        current_row_value = j
+                        for reverse_row in range(i):
+                            if matrix[reverse_row][j] == 1:
+                                done = False
+
+                if previous_row_value is None and current_row_value is not None:
+                    done = False
+                if previous_row_value is not None and current_row_value is not None and previous_row_value >= current_row_value:
+                    done = False
+
+    # we append to the list of echelon matrixes a new matrix
+    if done is True:
+        list_of_echelon_matrixes.append(matrix)
+        for row in matrix:
+            print(row)
+
+        print('\n')
+
+def is_valid(matrix, n, k):
+    """
+    we check if the matrix respects the validation steps
+    :param matrix: the current matrix
+    :param n: the number of the columns
+    :param k: the current index of the element in a way of (column * row)
+    :return:
+    """
+    row = k // n
+    if row == 0:
+        return True
+    column = k % n if k % n != 0 else 0
+    if column == 0:
+        if matrix[row][column] == 1:
+            return False
+    return True
+
+
+def is_solution(k, n, m):
+    """
+    if the k reached then of the matrix it means that we have a solution
+    :param k: the index of the current element
+    :param n: the number of columns
+    :param m: the number of the rows
+    :return:
+    """
+    if k == n * m:
+        return True
+    return False
+
+
+"""
+Now we do a simple backtracking now
+"""
+
+
+def backtracking(matrix, n, m, k, list_of_echelon_matrixes):
+    if is_solution(k, n, m):
+        print_solution(matrix, n, m, list_of_echelon_matrixes)
+    else:
+        for i in range(2):
+            row = k // n
+            column = k % n if k % n != 0 else 0
+            matrix[row][column] = i
+            if is_valid(matrix, n, k):
+                backtracking(matrix, n, m, k + 1, list_of_echelon_matrixes)
+                matrix[row][column] = 0
+
+
+
+n, m, matrix = init_matrix()
+list_of_echelon_matrixes = list()
+backtracking(matrix, n, m, 0, list_of_echelon_matrixes)
+print("\n")
+
+print(f" The number of matrixes ( M{m},{n} in Z2 ) in reduced echelon form is: {len(list_of_echelon_matrixes)}")
